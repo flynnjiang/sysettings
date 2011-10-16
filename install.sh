899f1150b065e1fbf5f3b8b865c77c2cead5c874
flynnjiang <i@jiangfeng.org>
2011-10-16 11:57:31 +0800

    edit install script

--- install.sh
+++ install.sh
@@ -1,8 +1,22 @@
 #!/bin/sh
 
+
+BACKUP_DIR=~/.sysettings
+
+# Do backup first
+if [ ! -d "$BACKUP_DIR" ]; then
+    mkdir  $BACKUP_DIR
+fi
+cp ~/.bash_profile ~/.bashrc $BACKUP_DIR
+cp ~/.vimrc $BACKUP_DIR
+cp -r ~/.vim $BACKUP_DIR
+
+
 # Install shell scripts.
-cp -r bash/* ~/
+cp bash/.bashrc bash/.bash_profile  ~/
+cp -r bash/.bashrc.d ~/
 
 
 # Install vim files.
-cp -r vim/* ~/
+cp vim/.vimrc ~/
+cp -r vim/.vim ~/
